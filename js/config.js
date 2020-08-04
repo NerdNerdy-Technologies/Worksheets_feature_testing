bfb1597c2eb65695f55cd57d45c3ca411ffcc147
Tulsani <38261746+Tulsani@users.noreply.github.com>
2020-08-04 17:24:59 +0530

    demo

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_yEPQl1Jlz', // e.g. us-east-2_uXboG5pAb
+        region: 'ap-south-1', // e.g. us-east-2
+		clientId: '2lii8tcci60dvqhd8oebp3qql9' //is this used anywhere?
+    },
+};
